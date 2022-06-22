7fc24371cab5177f2360fef08ba81c52d1c93624
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-22 18:53:14 +0300

    Concrete implementation of IReadRepository

---
+++ OnionArchitecture.Persistence/Repositories/Common/ReadRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Application.Enums;
+using OnionArchitecture.Application.Repositories.Common;
+using OnionArchitecture.Domain.Entities.Common;
+using OnionArchitecture.Persistence.Contexts;
+
+namespace OnionArchitecture.Persistence.Repositories.Common
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        private readonly OnionArchitectureDbContext _context;
+
+        public ReadRepository(OnionArchitectureDbContext context)
+        {
+            this._context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll(TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.AsNoTracking() : Table.AsQueryable();
+
+        public IQueryable<T> GetAllByFilter(Expression<Func<T, bool>> filter, TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.Where(filter).AsNoTracking() : Table.Where(filter);
+
+        public T? GetByFilter(Expression<Func<T, bool>> filter, TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.AsNoTracking().FirstOrDefault(filter) : Table.FirstOrDefault(filter);
+
+        public Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter, TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.AsNoTracking().FirstOrDefaultAsync(filter) : Table.FirstOrDefaultAsync(filter);
+
+        public T? GetById(Guid id, TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.AsNoTracking().FirstOrDefault(entity => entity.Id == id) : Table.FirstOrDefault(entity => entity.Id == id);
+
+        public Task<T?> GetByIdAsync(Guid id, TrackingState trackingState) => trackingState == TrackingState.NoTracking ? Table.AsNoTracking().FirstOrDefaultAsync(entity => entity.Id == id) : Table.FirstOrDefaultAsync(entity => entity.Id == id);
+    }
+}
