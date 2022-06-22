9247dfda77d9e4ff1e145571c763b1228fbbf551
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-22 17:06:43 +0300

    Add domain objects

---
+++ OnionArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using OnionArchitecture.Domain.Entities.Common;
+
+namespace OnionArchitecture.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public ICollection<Product> Products { get; set; } = default!;
+    }
+}
---
+++ OnionArchitecture.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using OnionArchitecture.Domain.Entities.Common;
+
+namespace OnionArchitecture.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = default!;
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; } = default!;
+    }
+}
