8a381ee8bd1dfc12d49c63f6f87272299f835368
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-22 17:27:21 +0300

    Add category interfaces for repositories

---
+++ OnionArchitecture.Application/Repositories/Categories/ICategoryReadRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnionArchitecture.Application.Repositories.Common;
+using OnionArchitecture.Domain.Entities;
+
+namespace OnionArchitecture.Application.Repositories.Categories
+{
+    public interface ICategoryReadRepository : IReadRepository<Category>
+    {
+    }
+}
