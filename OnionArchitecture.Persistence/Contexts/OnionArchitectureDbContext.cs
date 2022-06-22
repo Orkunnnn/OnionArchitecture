161701f29e24b5015c59ccc0c5aa69b5dbc9e49b
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-22 18:07:50 +0300

    Add dbcontext

---
+++ OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Domain.Entities;
+
+namespace OnionArchitecture.Persistence.Contexts
+{
+    public class OnionArchitectureDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = default!;
+        public DbSet<Category> Categories { get; set; } = default!;
+    }
+}
