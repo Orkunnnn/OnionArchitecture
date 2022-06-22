d0f08701bd1ba12d3048a7c2bc079e420244a179
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-22 17:24:01 +0300

    Add product interfaces for repositories

---
+++ OnionArchitecture.Application/Repositories/Products/IProductWriteRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnionArchitecture.Application.Repositories.Common;
+using OnionArchitecture.Domain.Entities;
+
+namespace OnionArchitecture.Application.Repositories.Products
+{
+    public interface IProductWriteRepository : IWriteRepository<Product>
+    {
+    }
+}
