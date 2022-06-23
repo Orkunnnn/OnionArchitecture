3554815c02b2f78975e812a366548ee7bf17f285
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 18:29:19 +0300

    Refactor change static to const

--- OnionArchitecture.Application/Constants/Products/Message.cs
+++ OnionArchitecture.Application/Constants/Products/Message.cs
@@ -2,12 +2,12 @@
 {
     public static class Message
     {
-        public static string ProvideProductId = "Please enter a product id.";
-        public static string ProvideProductName = "Please enter a product name.";
-        public static string ProvideProductPrice = "Please enter a price value.";
-        public static string ProvideProductStock = "Please enter a price stock.";
-        public static string InvalidProductNameLength = "Product name must be between 2-150 character.";
-        public static string InvalidProductPrice = "Product price must be greater than 0.";
-        public static string InvalidProductStock = "Product stock must be greater than or equal to 0.";
+        public const string ProvideProductId = "Please enter a product id.";
+        public const string ProvideProductName = "Please enter a product name.";
+        public const string ProvideProductPrice = "Please enter a price value.";
+        public const string ProvideProductStock = "Please enter a price stock.";
+        public const string InvalidProductNameLength = "Product name must be between 2-150 character.";
+        public const string InvalidProductPrice = "Product price must be greater than 0.";
+        public const string InvalidProductStock = "Product stock must be greater than or equal to 0.";
     }
 }
