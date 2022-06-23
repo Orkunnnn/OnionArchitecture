bf1bb7c180afa9ef21e2d95848ca92fd3cd8cb95
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:39:58 +0300

    Add messages for category

---
+++ OnionArchitecture.Application/Constants/Categories/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace OnionArchitecture.Application.Constants.Categories
+{
+    public static class Message
+    {
+        public const string ProvideCategoryId = "Please enter a category id.";
+        public const string ProvideCategoryName = "Please enter a category name.";
+        public const string InvalidCategoryName = "Please enter a valid category name.";
+    }
+}
