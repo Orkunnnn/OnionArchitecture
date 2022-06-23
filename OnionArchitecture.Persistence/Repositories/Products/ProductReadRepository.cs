0b284304d1da17eabd6f0e6535f931ba36fb040b
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:21:50 +0300

    Add ProductReadRepository for database reading operations

---
+++ OnionArchitecture.Persistence/Repositories/Products/ProductReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Application.Repositories.Products;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Persistence.Contexts;
+using OnionArchitecture.Persistence.Repositories.Common;
+
+namespace OnionArchitecture.Persistence.Repositories.Products
+{
+    public class ProductReadRepository : ReadRepository<Product>, IProductReadRepository
+    {
+        public ProductReadRepository(OnionArchitectureDbContext context) : base(context)
+        {
+        }
+    }
+}
