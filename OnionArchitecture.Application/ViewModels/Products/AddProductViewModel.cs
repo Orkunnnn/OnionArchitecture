21312df579ab5be0cb83f1d8c238e30cdbe9392e
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 14:04:41 +0300

    Add product view model for adding product

---
+++ OnionArchitecture.Application/ViewModels/Products/AddProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnionArchitecture.Application.ViewModels.Products
+{
+    public class AddProductViewModel
+    {
+        public string Name { get; set; } = default!;
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
