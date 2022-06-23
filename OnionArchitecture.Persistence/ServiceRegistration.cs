77b0cef0c4d6593619cb12415cf4f16b9fded72c
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:25:40 +0300

    Add AddPersistenceServices extension method to configure api services

---
+++ OnionArchitecture.Persistence/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchitecture.Application.Repositories.Products;
+using OnionArchitecture.Persistence.Contexts;
+using OnionArchitecture.Persistence.Repositories.Products;
+
+namespace OnionArchitecture.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<OnionArchitectureDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
