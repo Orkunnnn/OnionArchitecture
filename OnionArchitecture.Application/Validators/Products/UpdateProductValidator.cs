53aa737cf226b97bdb6f1c1fcba576258284c8eb
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 14:14:55 +0300

    Revert "Revert "Add product view model for updating the product""
    This reverts commit 07fb6f209940ac49d5e53f2a3bcb30a45285c95e.

---
+++ OnionArchitecture.Application/Validators/Products/UpdateProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OnionArchitecture.Application.ViewModels.Products;
+
+namespace OnionArchitecture.Application.Validators.Products
+{
+    public class UpdateProductValidator : AbstractValidator<UpdateProductViewModel>
+    {
+        public UpdateProductValidator()
+        {
+            RuleFor(x => x.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage("Please enter a id value.");
+            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage("Please enter a product name.").MinimumLength(2).MaximumLength(150).WithMessage("")
+        }
+    }
+}
