b5ccad984de4b18ea64e975c333d307dc2e34c42
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 14:32:09 +0300

    Add product validator for AddProductViewModel

--- OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
+++ OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
@@ -1,5 +1,5 @@
 ﻿using FluentValidation;
-using OnionArchitecture.Application.Constants;
+using OnionArchitecture.Application.Constants.Product;
 using OnionArchitecture.Application.ViewModels.Products;
 
 namespace OnionArchitecture.Application.Validators.Products
@@ -8,9 +8,9 @@ namespace OnionArchitecture.Application.Validators.Products
     {
         public AddProductValidator()
         {
-            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Messages.ProvideProductName).MaximumLength(150).MinimumLength(2).WithMessage(Messages.InvalidProductNameLength);
-            RuleFor(x => x.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Messages.ProvideProductPrice).GreaterThan(0).WithMessage(Messages.InvalidProductPrice);
-            RuleFor(x => x.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Messages.ProvideProductStock).GreaterThanOrEqualTo(0).WithMessage(Messages.InvalidProductStock);
+            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MaximumLength(150).MinimumLength(2).WithMessage(Message.InvalidProductNameLength);
+            RuleFor(x => x.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
+            RuleFor(x => x.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
         }
     }
 }
