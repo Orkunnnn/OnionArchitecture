93ba818580d1a1bbb7c1be31fce3ba27534ee688
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:40:33 +0300

    Add category validators for category view models

---
+++ OnionArchitecture.Application/Validators/Categories/AddCategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OnionArchitecture.Application.Constants.Categories;
+using OnionArchitecture.Application.ViewModels.Categories;
+
+namespace OnionArchitecture.Application.Validators.Categories
+{
+    public class AddCategoryValidator : AbstractValidator<AddCategoryViewModel>
+    {
+        public AddCategoryValidator()
+        {
+            RuleFor(c => c.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideCategoryName).MinimumLength(2).MaximumLength(50).WithMessage(Message.InvalidCategoryName);
+        }
+    }
+}
---
+++ OnionArchitecture.Application/Validators/Categories/UpdateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OnionArchitecture.Application.Constants.Categories;
+using OnionArchitecture.Application.ViewModels.Categories;
+
+namespace OnionArchitecture.Application.Validators.Categories
+{
+    public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryViewModel>
+    {
+        public UpdateCategoryValidator()
+        {
+            RuleFor(c => c.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideCategoryId);
+            RuleFor(c => c.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideCategoryName).MinimumLength(2).MaximumLength(50).WithMessage(Message.InvalidCategoryName);
+        }
+    }
+}
