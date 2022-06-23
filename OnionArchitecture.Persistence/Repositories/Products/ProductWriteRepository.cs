f6387ec994278088a14e4ea5a572fa48b85f7fb7
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:22:09 +0300

    Add ProductWriteRepository for database write operations

---
+++ OnionArchitecture.Persistence/Repositories/Products/ProductWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Application.Repositories.Products;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Persistence.Contexts;
+using OnionArchitecture.Persistence.Repositories.Common;
+
+namespace OnionArchitecture.Persistence.Repositories.Products
+{
+    public class ProductWriteRepository : WriteRepository<Product>, IProductWriteRepository
+    {
+        public ProductWriteRepository(OnionArchitectureDbContext context) : base(context)
+        {
+        }
+    }
+}
