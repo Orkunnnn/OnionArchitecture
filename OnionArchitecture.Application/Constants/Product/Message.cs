6296c0130f8a0a95c2150ca28f522cc87a779a56
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 14:31:17 +0300

    Add product message to avoid magic strings

---
+++ OnionArchitecture.Application/Constants/Product/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace OnionArchitecture.Application.Constants.Product
+{
+    public static class Message
+    {
+        public static string ProvideProductId = "Please enter a product id.";
+        public static string ProvideProductName = "Please enter a product name.";
+        public static string ProvideProductPrice = "Please enter a price value.";
+        public static string ProvideProductStock = "Please enter a price stock.";
+        public static string InvalidProductNameLength = "Product name must be between 2-150 character.";
+        public static string InvalidProductPrice = "Product price must be greater than 0.";
+        public static string InvalidProductStock = "Product stock must be greater than or equal to 0.";
+    }
+}
