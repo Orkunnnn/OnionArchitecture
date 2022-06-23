54e7508b6922df399e4c9bd1a85477a92f5215b7
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:40:53 +0300

    Refactor change x to p

--- OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
+++ OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
@@ -8,9 +8,9 @@ namespace OnionArchitecture.Application.Validators.Products
     {
         public AddProductValidator()
         {
-            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MaximumLength(150).MinimumLength(2).WithMessage(Message.InvalidProductNameLength);
-            RuleFor(x => x.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
-            RuleFor(x => x.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
+            RuleFor(p => p.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MaximumLength(150).MinimumLength(2).WithMessage(Message.InvalidProductNameLength);
+            RuleFor(p => p.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
+            RuleFor(p => p.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
         }
     }
 }
--- OnionArchitecture.Application/Validators/Products/UpdateProductValidator.cs
+++ OnionArchitecture.Application/Validators/Products/UpdateProductValidator.cs
@@ -8,10 +8,10 @@ namespace OnionArchitecture.Application.Validators.Products
     {
         public UpdateProductValidator()
         {
-            RuleFor(x => x.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductId);
-            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MinimumLength(2).MaximumLength(150).WithMessage(Message.InvalidProductNameLength);
-            RuleFor(x => x.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.InvalidProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
-            RuleFor(x => x.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
+            RuleFor(p => p.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductId);
+            RuleFor(p => p.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MinimumLength(2).MaximumLength(150).WithMessage(Message.InvalidProductNameLength);
+            RuleFor(p => p.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.InvalidProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
+            RuleFor(p => p.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
         }
     }
 }
