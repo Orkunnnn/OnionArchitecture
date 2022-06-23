adfa1766ddb74acf4931e8e630620d164bbb2043
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:20:41 +0300

    Add constructor for DbContext with options parameter

--- OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
+++ OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
@@ -5,6 +5,10 @@ namespace OnionArchitecture.Persistence.Contexts
 {
     public class OnionArchitectureDbContext : DbContext
     {
+        public OnionArchitectureDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Product> Products { get; set; } = default!;
         public DbSet<Category> Categories { get; set; } = default!;
     }
