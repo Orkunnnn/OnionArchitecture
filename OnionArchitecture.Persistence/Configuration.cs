b5bc5b795ba63b7bd10350c0661d3443a6010e7d
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:41:49 +0300

    Refactor change directory

--- OnionArchitecture.Persistence/Configuration.cs
+++ OnionArchitecture.Persistence/Configuration.cs
@@ -9,7 +9,8 @@ namespace OnionArchitecture.Persistence
             get
             {
                 ConfigurationManager configurationManager = new();
-                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../OnionArchitecture.Api")).AddJsonFile("appsettings.json");
+                configurationManager.SetBasePath(Directory.GetCurrentDirectory());
+                configurationManager.AddJsonFile("appsettings.json").AddUserSecrets("7bc21b82-df20-428b-9607-cbe0a12ad0e1");
                 return configurationManager.GetConnectionString("SqlServer");
             }
         }
