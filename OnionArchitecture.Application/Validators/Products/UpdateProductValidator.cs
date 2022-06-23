8b43d0a7e61d0d4d0a27f7666ec2d4f23ee3b010
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 14:32:22 +0300

    Add product validator for UpdateProductViewModel

--- OnionArchitecture.Application/Validators/Products/UpdateProductValidator.cs
+++ OnionArchitecture.Application/Validators/Products/UpdateProductValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using OnionArchitecture.Application.Constants.Product;
 using OnionArchitecture.Application.ViewModels.Products;
 
 namespace OnionArchitecture.Application.Validators.Products
@@ -7,8 +8,10 @@ namespace OnionArchitecture.Application.Validators.Products
     {
         public UpdateProductValidator()
         {
-            RuleFor(x => x.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage("Please enter a id value.");
-            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage("Please enter a product name.").MinimumLength(2).MaximumLength(150).WithMessage("")
+            RuleFor(x => x.Id).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductId);
+            RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MinimumLength(2).MaximumLength(150).WithMessage(Message.InvalidProductNameLength);
+            RuleFor(x => x.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.InvalidProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
+            RuleFor(x => x.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
         }
     }
 }
