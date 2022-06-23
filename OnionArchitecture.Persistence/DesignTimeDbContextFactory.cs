01b5f0e7f81f5b995c9004b7c43793715b7316ff
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:23:45 +0300

    Add DesignTimeDbContextFactory for command line support to create and update the database

---
+++ OnionArchitecture.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using OnionArchitecture.Persistence.Contexts;
+
+namespace OnionArchitecture.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<OnionArchitectureDbContext>
+    {
+        public OnionArchitectureDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<OnionArchitectureDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
