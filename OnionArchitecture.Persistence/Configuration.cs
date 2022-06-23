2b392a4fe24c1a75d1dcbb9bc35b9022d58bdeb3
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-23 20:22:35 +0300

    Add static configuration class for handle database connection string

---
+++ OnionArchitecture.Persistence/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OnionArchitecture.Persistence
+{
+    public static class Configuration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../OnionArchitecture.Api")).AddJsonFile("appsettings.json");
+                return configurationManager.GetConnectionString("SqlServer");
+            }
+        }
+    }
+}
