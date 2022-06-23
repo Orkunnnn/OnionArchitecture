3db2eba34ab95c216f55960b140a653d90dd57cd
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:31:39 +0300

    Refactor implement base method

--- OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
+++ OnionArchitecture.Persistence/Contexts/OnionArchitectureDbContext.cs
@@ -13,6 +13,14 @@ namespace OnionArchitecture.Persistence.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
         }
 
         public DbSet<Product> Products { get; set; } = default!;
