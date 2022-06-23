a8c2e794253e7a42443d6ec4c4fa7a79842af3ea
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-24 01:41:18 +0300

    Add category repositories

---
+++ OnionArchitecture.Persistence/Repositories/Categories/CategoryReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Application.Repositories.Categories;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Persistence.Contexts;
+using OnionArchitecture.Persistence.Repositories.Common;
+
+namespace OnionArchitecture.Persistence.Repositories.Categories
+{
+    public class CategoryReadRepository : ReadRepository<Category>, ICategoryReadRepository
+    {
+        public CategoryReadRepository(OnionArchitectureDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ OnionArchitecture.Persistence/Repositories/Categories/CategoryWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Application.Repositories.Categories;
+using OnionArchitecture.Domain.Entities;
+using OnionArchitecture.Persistence.Contexts;
+using OnionArchitecture.Persistence.Repositories.Common;
+
+namespace OnionArchitecture.Persistence.Repositories.Categories
+{
+    public class CategoryWriteRepository : WriteRepository<Category>, ICategoryWriteRepository
+    {
+        public CategoryWriteRepository(OnionArchitectureDbContext context) : base(context)
+        {
+        }
+    }
+}
