7be5cf58cce01b769d9cd0012a9b825ae5ce77aa
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-25 00:28:30 +0300

    Refactor set default value for tracking state

--- OnionArchitecture.Application/Repositories/Common/IReadRepository.cs
+++ OnionArchitecture.Application/Repositories/Common/IReadRepository.cs
@@ -6,11 +6,11 @@ namespace OnionArchitecture.Application.Repositories.Common
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll(TrackingState trackingState);
-        IQueryable<T> GetAllByFilter(Expression<Func<T, bool>> filter, TrackingState trackingState);
+        IQueryable<T> GetAll(TrackingState trackingState = TrackingState.WithTracking);
+        IQueryable<T> GetAllByFilter(Expression<Func<T, bool>> filter, TrackingState trackingState = TrackingState.WithTracking);
         T? GetByFilter(Expression<Func<T, bool>> filter, TrackingState trackingState);
-        T? GetById(Guid id, TrackingState trackingState);
-        Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter, TrackingState trackingState);
-        Task<T?> GetByIdAsync(Guid id, TrackingState trackingState);
+        T? GetById(Guid id, TrackingState trackingState = TrackingState.WithTracking);
+        Task<T?> GetByFilterAsync(Expression<Func<T, bool>> filter, TrackingState trackingState = TrackingState.WithTracking);
+        Task<T?> GetByIdAsync(Guid id, TrackingState trackingState = TrackingState.WithTracking);
     }
 }
