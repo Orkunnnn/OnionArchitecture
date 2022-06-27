d0928b80645ba9afa6db0fdb8d9ef8cf63cbf70a
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-27 20:47:32 +0300

    Add controller class for products

---
+++ OnionArchitecture.Api/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Application.CQRS.Commands.CreateProduct;
+using OnionArchitecture.Application.CQRS.Queries.GetAllProducts;
+using System.Net;
+
+namespace OnionArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _mediator.Send(new GetAllProductsQueryRequest());
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommandRequest createProductCommandRequest)
+        {
+            await _mediator.Send(createProductCommandRequest);
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+    }
+}
