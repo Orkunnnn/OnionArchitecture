7e165d4efeb1b66ba0e61cfb8051349a16eb7d6c
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-27 20:45:46 +0300

    Add mapper profile for product

---
+++ OnionArchitecture.Application/Mappings/AutoMapper/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OnionArchitecture.Application.ViewModels.Products;
+using OnionArchitecture.Domain.Entities;
+
+namespace OnionArchitecture.Application.Mappings.AutoMapper
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, GetProductViewModel>().ForMember(p => p.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+        }
+    }
+}
