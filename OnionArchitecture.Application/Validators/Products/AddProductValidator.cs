ae9e6ea844512b9e282a43899f80d2796d605e1e
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-27 21:15:02 +0300

    Add scale precison validation for price

--- OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
+++ OnionArchitecture.Application/Validators/Products/AddProductValidator.cs
@@ -9,7 +9,7 @@ namespace OnionArchitecture.Application.Validators.Products
         public AddProductValidator()
         {
             RuleFor(p => p.Name).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductName).MaximumLength(150).MinimumLength(2).WithMessage(Message.InvalidProductNameLength);
-            RuleFor(p => p.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).WithMessage(Message.InvalidProductPrice);
+            RuleFor(p => p.Price).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductPrice).GreaterThan(0).ScalePrecision(2, 8, true).WithMessage(Message.InvalidProductPrice);
             RuleFor(p => p.Stock).Cascade(CascadeMode.Stop).NotEmpty().NotNull().WithMessage(Message.ProvideProductStock).GreaterThanOrEqualTo(0).WithMessage(Message.InvalidProductStock);
         }
     }
