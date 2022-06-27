cadd48a76b47ff3ff81f825fa367e914ad5ab7e8
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-27 20:44:14 +0300

    Add dependency injection for automapper

---
+++ OnionArchitecture.Application/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnionArchitecture.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            var serviceRegistrationType = typeof(ServiceRegistration);
+            services.AddMediatR(serviceRegistrationType);
+            services.AddAutoMapper(serviceRegistrationType);
+        }
+    }
+}
