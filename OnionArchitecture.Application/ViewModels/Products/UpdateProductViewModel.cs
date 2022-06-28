423cb64a3d5fe21644823e4a22b61263472dd5d4
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-28 17:06:06 +0300

    Add view model for updating the product

--- OnionArchitecture.Application/ViewModels/Products/UpdateProductViewModel.cs
+++ OnionArchitecture.Application/ViewModels/Products/UpdateProductViewModel.cs
@@ -3,6 +3,7 @@
     public class UpdateProductViewModel
     {
         public Guid Id { get; set; }
+        public Guid CategoryId { get; set; }
         public string Name { get; set; } = default!;
         public int Stock { get; set; }
         public decimal Price { get; set; }
