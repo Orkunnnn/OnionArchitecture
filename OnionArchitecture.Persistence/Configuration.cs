bcdc8e86e7884330f239f65cd9c0471713a8424b
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-29 03:16:33 +0300

    Commented for test purposes

--- OnionArchitecture.Persistence/Configuration.cs
+++ OnionArchitecture.Persistence/Configuration.cs
@@ -1,18 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
-
-namespace OnionArchitecture.Persistence
+﻿namespace OnionArchitecture.Persistence
 {
     public static class Configuration
     {
-        public static string ConnectionString
-        {
-            get
-            {
-                ConfigurationManager configurationManager = new();
-                configurationManager.SetBasePath(Directory.GetCurrentDirectory());
-                configurationManager.AddJsonFile("appsettings.json").AddUserSecrets("7bc21b82-df20-428b-9607-cbe0a12ad0e1");
-                return configurationManager.GetConnectionString("SqlServer");
-            }
-        }
+
+        //public static string ConnectionString
+        //{
+        //    get
+        //    {
+        //        ConfigurationManager configurationManager = new();
+        //        configurationManager.SetBasePath(Directory.GetCurrentDirectory());
+        //        configurationManager.AddJsonFile("appsettings.json").AddUserSecrets("7bc21b82-df20-428b-9607-cbe0a12ad0e1");
+        //        return configurationManager.GetConnectionString("SqlServer");
+        //    }
+        //}
     }
 }
