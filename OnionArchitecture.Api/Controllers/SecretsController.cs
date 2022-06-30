9b7e101e1cd3b910d75483611e83acdc007d44f7
Orkun Konak <orkunkonak1414@gmail.com>
2022-06-30 18:12:11 +0300

    Test controller for api

---
+++ OnionArchitecture.Api/Controllers/SecretsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnionArchitecture.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("/test")]
+        public IActionResult Test()
+        {
+            return Ok("Works");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_configuration["MySecret"]);
+        }
+    }
+}
