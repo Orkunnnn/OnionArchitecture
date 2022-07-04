3108fbfb718458c7591d8c731bb50643c1c607f1
Orkun Konak <orkunkonak1414@gmail.com>
2022-07-04 16:09:28 +0300

    Implement ApiKeyMiddleware

---
+++ OnionArchitecture.Api/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace OnionArchitecture.Api.Middlewares
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public ApiKeyMiddleware(IConfiguration configuration, RequestDelegate next)
+        {
+            _configuration = configuration;
+            _next = next;
+        }
+
+        private const string ApiKeyHeader = "ApiKey";
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeader, out var headerApiKey))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("Api key is not provided");
+                return;
+            }
+            var apiKey = _configuration[ApiKeyHeader];
+            if (!apiKey.Equals(headerApiKey))
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+            await _next(context);
+        }
+    }
+}
