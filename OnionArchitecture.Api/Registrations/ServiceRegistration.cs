288cf633d1bdc9c980bc1d574877a04c8651222a
Orkun Konak <orkunkonak1414@gmail.com>
2022-07-05 04:04:11 +0300

    Add reflection to register the services

---
+++ OnionArchitecture.Api/Registrations/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using OnionArchitecture.Application.Registrations;
+
+namespace OnionArchitecture.Api.Registrations
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApiRegistrations(this IServiceCollection services, IConfiguration configuration)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var registrations = assembly.ExportedTypes.Where(x => typeof(IRegistration).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(Activator.CreateInstance).Cast<IRegistration>().ToList();
+                registrations.ForEach(registration => registration.Register(services, configuration));
+            }
+
+            return services;
+        }
+    }
+}
